d839676e86a6507e435ffc58c9bf27248de6f263
rxssula <rxssul.aitkali@gmail.com>
2025-11-10 13:16:21 +0500

    [init]: initialize shadcn

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,39 +1,18 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const onClick = () => {
+    alert('AMAN LOH')
+  }
+
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div className="flex min-h-screen min-w-screen justify-center items-center">
+      <Button onClick={onClick}>Click Me!</Button>
     </div>
   )
 }
