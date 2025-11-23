5c81373e820d76fe4ce6a353f9de738c844fdf43
rxssula <rxssul.aitkali@gmail.com>
2025-11-23 21:18:40 +0500

    [feat]: prepare for vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 import tanstackRouter from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
@@ -9,6 +10,7 @@ import { resolve } from 'node:path'
 export default defineConfig({
   plugins: [
     tanstackRouter({ autoCodeSplitting: true }),
+    nitro(),
     viteReact(),
     tailwindcss(),
   ],
