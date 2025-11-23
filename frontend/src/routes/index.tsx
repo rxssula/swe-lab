becd60131e9397ebe16159c33954ef4f0159efb4
rxssula <rxssul.aitkali@gmail.com>
2025-11-23 19:47:43 +0500

    [feat]: redirect to auth so that no home page exist

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,18 +1,9 @@
-import { Button } from '@/components/ui/button'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: App,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/auth',
+    })
+  },
 })
-
-function App() {
-  const onClick = () => {
-    alert('AMAN LOH')
-  }
-
-  return (
-    <div className="flex min-h-screen min-w-screen justify-center items-center">
-      <Button onClick={onClick}>Click Me!</Button>
-    </div>
-  )
-}
